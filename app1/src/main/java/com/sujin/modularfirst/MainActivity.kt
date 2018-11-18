a4492e819cea748c1950cf93628843c3e7cc5169
Sujin <sujin.v2px@gmail.com>
2018-11-18 12:53:53 +0545

    dynamic feature test

--- app1/src/main/java/com/sujin/modularfirst/MainActivity.kt
+++ app1/src/main/java/com/sujin/modularfirst/MainActivity.kt
@@ -2,6 +2,8 @@ package com.sujin.modularfirst
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
+import com.google.android.play.core.splitinstall.SplitInstallRequest
 import com.sujin.posts.ui.posts.PostsActivity
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         startActivity(PostsActivity.getIntent(this))
+
+        val splitInstallManager = SplitInstallManagerFactory.create(this)
+        val request = SplitInstallRequest.newBuilder()
+            .addModule("dynamic_feature")
+            .build()
+
+        splitInstallManager.startInstall(request)
+            .addOnSuccessListener { sessionId -> }
+            .addOnFailureListener {}
     }
 }
